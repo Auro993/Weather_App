06424991ce6cbbedd464685fc6aa7cd27eb434b7
Aurosmita Sahoo <sahooaurosmita204@gmail.com>
2026-01-08 00:21:14 +0530

    Update activate.bat

--- activate.bat
+++ activate.bat
@@ -26,9 +26,9 @@ if not defined _OLD_VIRTUAL_PATH set _OLD_VIRTUAL_PATH=%PATH%
 
 set PATH=%VIRTUAL_ENV%\Scripts;%PATH%
 set VIRTUAL_ENV_PROMPT=(venv) 
-
 :END
 if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" %_OLD_CODEPAGE% > nul
     set _OLD_CODEPAGE=
 )
+
